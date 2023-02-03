ce038b0fd0db48791fd53d9b443bc9dea7912fa6
Mak-3 <75625675+Mak-3@users.noreply.github.com>
2023-02-03 17:52:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,15 +44,4 @@ function newElement() {
     counter.innerHTML = total;
   }
   document.getElementById("myInput").value = "";
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
 }
